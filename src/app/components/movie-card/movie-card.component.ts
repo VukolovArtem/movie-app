739625b95d3a80b0116edb53fc3f9f5f1685e2a2
Vukolov Artem <vuko.ram55@gmail.com>
2024-06-17 19:35:10 +0300

    update mivie card

--- src/app/components/movie-card/movie-card.component.ts
+++ src/app/components/movie-card/movie-card.component.ts
@@ -3,16 +3,28 @@ import { EventEmitter, OnInit } from '@angular/core';
 import {} from '@angular/core';
 import { Component, Input, Output } from '@angular/core';
 import { DateFormatPipe } from 'app/pipes/date-format.pipe';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { RatingModule } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-movie-card',
   standalone: true,
-  imports: [CommonModule, DateFormatPipe],
+  imports: [
+    CommonModule,
+    DateFormatPipe,
+    CardModule,
+    ButtonModule,
+    RatingModule,
+    FormsModule,
+  ],
   templateUrl: './movie-card.component.html',
   styleUrl: './movie-card.component.scss',
 })
 export class MovieCardComponent {
   @Input() movie: any;
+  @Input() maxRating: any;
   @Input() isFavorite: boolean = false;
 
   @Output() addToFavorite = new EventEmitter<any>();
