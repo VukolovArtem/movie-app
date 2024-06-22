ff4a1af38d9da364f6c10d99e4d56c0c59c2f42a
Vukolov Artem <vuko.ram55@gmail.com>
2024-06-22 18:07:55 +0300

    add routers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { MovieDetailPageComponent } from './pages/movie-detail-page/movie-detail-page.component';
+import { MovieListPageComponent } from './pages/movie-list-page/movie-list-page.component';
+import { MovieGuard } from '@app/guards/movie.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MovieListPageComponent },
+  {
+    path: 'movie/:id',
+    canActivate: [MovieGuard],
+    component: MovieDetailPageComponent,
+  },
+];
